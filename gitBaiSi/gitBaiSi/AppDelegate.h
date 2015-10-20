573d392dc16aa73b9cb2872405bc27f811825fad
xiaoxu <xiaoxu@xiaoxudemacbook-pro.local>
2015-10-20 16:08:48 +0800

    初始化文件

---
+++ gitBaiSi/gitBaiSi/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gitBaiSi
+//
+//  Created by xiaoxu on 15/10/20.
+//  Copyright © 2015年 xiaomage. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ gitBaiSi/gitBaiSi/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gitBaiSi
+//
+//  Created by xiaoxu on 15/10/20.
+//  Copyright © 2015年 xiaomage. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
